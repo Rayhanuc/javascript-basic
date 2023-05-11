50b1ea0465ca45ce9e99eb43ccee95cf57ada47f
Rayhanuc <rayhanctg4@gmail.com>
2023-05-12 01:51:28 +0600

    JavaScript basic first commit

---
+++ part-2/assets/js/script.js
@@ -0,0 +1,8 @@
+console.log(10 + 10);
+rayhanUddin = 10;
+console.log(rayhanUddin);
+console.log(typeof(rayhanUddin));
+rayhanUddin = "Rayhan Uddin";
+console.log(rayhanUddin);
+
+console.log(typeof(rayhanUddin));
