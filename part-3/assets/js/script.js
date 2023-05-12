dafb303c57e37cc07bc6354f879d554ded2510d3
Rayhanuc <rayhanctg4@gmail.com>
2023-05-12 16:23:12 +0600

    JavaScript Operator

---
+++ part-3/assets/js/script.js
@@ -0,0 +1,22 @@
+console.log(5 + 10);
+console.log(5 * 10);
+console.log(20 % 7);
+var number = 20;
+number++;
+console.log(number);
+console.log(10 == 10);
+console.log(10 == 23);
+console.log(10 != 20);
+console.log(10 > 20);
+console.log(10 > 5);
+console.log(10 >= 30);
+console.log(10 >= 7);
+console.log(10 >= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+console.log(10 == '10');
+console.log(10 === '10');
+
+
+
