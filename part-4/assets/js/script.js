4ec0b2bc6fb510ff0539b76ae0e5331a93a30f60
Rayhanuc <rayhanctg4@gmail.com>
2023-05-15 16:35:59 +0600

    Conditional Statements

---
+++ part-4/assets/js/script.js
@@ -0,0 +1,31 @@
+
+if(8>5) {
+  console.log("This should be displayed");
+}
+if(3>5) {
+  console.log("This should be displayed");
+}
+var test = 8;
+if(test == 8) {
+  console.log("This should be displayed");
+}
+if(test == '8') {
+  console.log("This should be displayed");
+}
+if(test === '8') {
+  console.log("This should be displayed");
+}
+
+if(test == 10) {
+  console.log("This should be displayed");
+} else {
+  console.log("Test value is not eight");
+}
+if(test == 10) {
+  console.log("This should be displayed");
+} else if (test == 9) {
+  console.log("This should be displayed");
+}
+else {
+  console.log("Test value is not eight");
+}
