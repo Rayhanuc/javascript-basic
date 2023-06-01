b5b7ab4a9d15d4361d697f08ac22d8fad3272738
Rayhanuc <rayhanctg4@gmail.com>
2023-06-02 02:10:05 +0600

    Switch Loop

---
+++ part-5/assets/js/script.js
@@ -0,0 +1,83 @@
+// Switch Loop
+
+var day = 'Saturday';
+
+switch (day) {
+  case 'Saturday':
+    console.log("This is saturday");
+    break;
+
+  case 'Sunday':    
+    console.log("This is Holiday");
+    break;
+    
+  case 'Friday':    
+    console.log("This is friday");
+    break;
+
+  default:
+    console.log("This is my default day");
+    break;
+}
+
+var day = 'Sunday';
+
+switch (day) {
+  case 'Saturday':
+    console.log("This is saturday");
+    break;
+
+  case 'Sunday':    
+    console.log("This is Holiday");
+    break;
+    
+  case 'Friday':    
+    console.log("This is friday");
+    break;
+
+  default:
+    console.log("This is my default day");
+    break;
+}
+
+
+var day = 'Friday';
+
+switch (day) {
+  case 'Saturday':
+    console.log("This is saturday");
+    break;
+
+  case 'Sunday':    
+    console.log("This is Holiday");
+    break;
+    
+  case 'Friday':    
+    console.log("This is friday");
+    break;
+
+  default:
+    console.log("This is my default day");
+    break;
+}
+
+
+var day = '';
+
+switch (day) {
+  case 'Saturday':
+    console.log("This is saturday");
+    break;
+
+  case 'Sunday':    
+    console.log("This is Holiday");
+    break;
+    
+  case 'Friday':    
+    console.log("This is friday");
+    break;
+
+  default:
+    console.log("This is my default day");
+    break;
+}
