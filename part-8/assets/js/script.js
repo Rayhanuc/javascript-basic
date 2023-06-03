19c0a9abd587e164d824ef2efb31ea453a477da2
Rayhanuc <rayhanctg4@gmail.com>
2023-06-04 03:12:51 +0600

    JS Object

---
+++ part-8/assets/js/script.js
@@ -0,0 +1,13 @@
+// JS Array
+
+var info = {
+  name: 'Hamid',
+  age: 45,
+  address: 'Chittagong',
+  profession: 'Teacher',
+  hello: alert("Hello dear")
+}
+
+info.hello
+console.log(info)
+console.log(info.profession)
