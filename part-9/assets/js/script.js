48da2c1e9ce616d6824956a1cf20e35c477f754b
Rayhanuc <rayhanctg4@gmail.com>
2023-06-06 00:21:49 +0600

    JS Function

---
+++ part-9/assets/js/script.js
@@ -0,0 +1,16 @@
+// JS Function
+
+function functionName() {
+  console.log("My name is Function.");
+}
+
+console.log(functionName());
+
+
+function sumMechine(a, b) {
+  var sum = a + b;
+  return sum;
+}
+
+console.log(sumMechine(20, 40));
+
